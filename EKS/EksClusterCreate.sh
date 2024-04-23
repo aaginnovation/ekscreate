c5065dec4867fd9662961c8d36168f6753cde1af
houseofposh01 <105612588+houseofposh01@users.noreply.github.com>
2024-04-23 14:58:41 -0500

    Update EksClusterCreate.sh

--- EKS/EksClusterCreate.sh
+++ EKS/EksClusterCreate.sh
@@ -5,11 +5,18 @@ unzip awscliv2.zip
 sudo ./aws/install
 
 #Install Kubectl
-sudo curl --silent --location -o /usr/local/bin/kubectl \
-   https://s3.us-west-2.amazonaws.com/amazon-EKS/1.21.5/2022-01-21/bin/linux/amd64/kubectl
-   
-#Change mode of kubectl
-sudo chmod +x /usr/local/bin/kubectl
+curl -LO https://dl.k8s.io/release/v1.27.0/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/v1.27.0/bin/linux/amd64/kubectl.sha256"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+#If you do not have root access on the target system,
+#you can still install kubectl to the ~/.local/bin directory:
+
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+# and then append (or prepend) ~/.local/bin to $PATH
 
 #Export Account variable
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account) 
